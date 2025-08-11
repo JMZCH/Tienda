57db65f48fe30f597234e490fe5850b87e534ca8
JMZCH <armando.martinez.chaves@gmail.com>
2025-08-11 15:01:17 -0600

    Semana 5

---
+++ src/main/java/domain/Categoria.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+/**
+ *
+ * @author arman
+ */
+@Data
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private long IdCategoria;
+    private String descripcion;
+    @Column(name = "ruta_imagen")
+    private String rutaImagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+}
---
+++ src/main/java/service/CategoriaService.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package service;
+import com.tienda.domain.Categoria;
+import java.util.List;
+/**
+ *
+ * @author arman
+ */
+public interface CategoriaService {
+    public List<Categoria> getCategorias(boolean activos);
+}
